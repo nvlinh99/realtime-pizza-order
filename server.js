12a43451b7d619d2d27a9b101ca05ed44fec0f35
Linh Nguyen Vu <nguyenvulinh99@gmail.com>
2020-08-18 15:09:19 +0700

    Initial structure

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	res.render('home')
+});
+
+//Setup template engine
+app.use(expressLayout);
+app.set('views', path.join(__dirname, './resources/views'));
+app.set('view engine', 'ejs');
+
+
+app.listen(PORT, () => {console.log(`Server is listening on port ${PORT}`);});

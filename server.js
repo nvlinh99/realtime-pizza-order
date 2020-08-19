2d59a2256a09ba79d57a2f30257787a828f301f0
Linh Nguyen Vu <nguyenvulinh99@gmail.com>
2020-08-19 09:46:23 +0700

    Add Cart, Auth

--- server.js
+++ server.js
@@ -8,15 +8,28 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //Assets
-app.use(express.static('public'));	
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-	res.render('home')
+  res.render('home');
+});
+
+app.get('/cart', (req, res) => {
+  res.render('customers/cart');
+});
+
+app.get('/login', (req, res) => {
+  res.render('auth/login');
+});
+
+app.get('/register', (req, res) => {
+  res.render('auth/register');
 });
 
 //Setup template engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname, './resources/views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); 
 
-
-app.listen(PORT, () => {console.log(`Server is listening on port ${PORT}`);});
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
